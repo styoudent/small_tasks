3991057147c6c0450ade9435fef01ba44de3a4c2
styoudent <styoudent@int.pl>
2023-03-02 22:12:15 +0100

    1. new repo 2. base of TwoSum problem

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import two_sum.TwoSum;
+
+public class Main {
+    public static void main(String[] args) {
+        final int[] numsToCheck = {1,2,3,7,11,15};
+        final int targetSum = 14;
+        TwoSum.numChecker(numsToCheck);
+    }
+}
---
+++ src/two_sum/TwoSum.java
@@ -0,0 +1,10 @@
+package two_sum;
+
+public class TwoSum {
+    public static void numChecker(int[] numsToCheck) {
+        for (int i : numsToCheck) {
+            System.out.println(i);
+        }
+    }
+
+}
