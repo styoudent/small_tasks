97435f17046782712c860c967632eb8050e3db2d
styoudent <styoudent@int.pl>
2023-03-02 23:08:19 +0100

    added body to numChecker

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import two_sum.TwoSum;
 
 public class Main {
     public static void main(String[] args) {
-        final int[] numsToCheck = {1,2,3,7,11,15};
-        final int targetSum = 14;
-        TwoSum.numChecker(numsToCheck);
+        final int[] givenNumbers = {1,2,3,7,11,15};
+        final int targetSum = 30;
+        TwoSum.numChecker(givenNumbers, targetSum);
     }
 }
--- src/two_sum/TwoSum.java
+++ src/two_sum/TwoSum.java
@@ -1,10 +1,15 @@
 package two_sum;
 
 public class TwoSum {
-    public static void numChecker(int[] numsToCheck) {
-        for (int i : numsToCheck) {
-            System.out.println(i);
+    public static void numChecker(int[] numsToCheck, int targetSum) {
+        for (int i=0; i<numsToCheck.length; i++) {
+            for (int j=0; j<numsToCheck.length; j++)
+                if ((numsToCheck[i] + numsToCheck[j]) == targetSum) {
+                    System.out.println("Indexes: " + i + ", " + j);
+                    return;
+                }
         }
+        System.out.println("There is no solution in this set of numbers");
     }
 
 }
